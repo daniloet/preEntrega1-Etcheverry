c945345db40ef5039a85ca2d0ade9088f1aa508d
Danilo Etcheverry <daniloetcheverry@icloud.com>
2023-11-22 12:19:27 -0300

    se creó simulador de comisiones

--- main.js
+++ main.js
@@ -11,9 +11,36 @@ let tiempoLimite = prompt("Ingrese en cuantos minutos suena el temporizador")
 tiempoLimite = parseInt(tiempoLimite)
 console.log("Tiempo del minuto 1 al " + tiempoLimite + ":")
 
-for (i = 0; i <= tiempoLimite; i +=1){
+for (i = 0; i <= tiempoLimite; i += 1) {
+
     console.log(i);
-    
-    
-} 
+
+}
 console.log("sonar alarma");
+
+
+
+
+function calcularComision(ventas) {
+    const tasaComision = 0.01;
+
+    const comision = ventas * tasaComision;
+
+    return comision;
+
+}
+function simular() {
+
+    let ventas = prompt("ingrese las ventas mensuales:");
+
+if (isNaN(ventas)){
+    alert("por favor, ingrese un numero valido");
+    return;
+}
+
+ventas = parseInt(ventas);
+
+const comision =  calcularComision(ventas);
+alert("Monto de ventas: $" + ventas + " Comision: $" + comision);
+}
+simular();
